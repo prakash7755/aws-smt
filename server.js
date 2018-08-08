6d3e90aa43df3f6155535b6f3da10156ba85d9b5
niharika <niharika@veniteck.com>
2018-08-08 03:05:49 -0700

    test

--- server.js
+++ server.js
@@ -15,15 +15,47 @@ app.use(bodyParsere.json());
 // mob or web or device Insert Value
 
 app.post('/SMT-400', async(req, res) => {
-    const { mac, equip_mode, fan_speed, temp_cool, temp_heat, occupied, relay_status, wifi_lost } = req.body;
+    const { mac, MsgID, equip_mode, fan_speed, temp_cool, temp_heat, occupied, relay_status, wifi_lost } = req.body;
     try {
 
         if (req.body.mac.length !== 12) {
             return res.send('MacId Must Be 12 Characters');
         }
-        const findValue = await DeviceParam.find({ mac });
-        const findLastValue = findValue.reverse()[0];
 
+        // New device 
+
+
+        const findValue = await DeviceParam.find({ mac });
+        if (!findValue[0]) {
+            const deviceParam = new DeviceParam(req.body);
+            const result = await deviceParam.save();
+            return res.json({ result: "ok" })
+        }
+
+        // Compare Device Value And Collection Values
+
+        const findLastValue = findValue.reverse()[0];
+        if (findLastValue.MsgID !== '') {
+            let MsgID = findLastValue.MsgID
+            const overRight = await DeviceParam.findOneAndUpdate({ mac, MsgID: findLastValue.MsgID }, { $set: { MsgID: '' } }, { new: true });
+            var arrayValue = ['mac', 'equip_mode', 'fan_speed', 'temp_cool', 'temp_heat', 'occupied', 'relay_status', 'wifi_lost'];
+            const newArray = [
+                ['MsgID', MsgID]
+            ]
+            arrayValue.map((data) => {
+                if (!(findLastValue[data] == req.body[data])) {
+                    return newArray.push([data, overRight[data]])
+                }
+            });
+            if (newArray.length > 1) {
+                let finalResult = _.object(newArray)
+                return res.json(finalResult);
+            }
+            return res.json({ result: 'ok' });
+        }
+   
+
+   // Every 20Sec Insert values In Collection
         if (equip_mode === findLastValue.equip_mode && fan_speed === findLastValue.fan_speed &&
             temp_cool === findLastValue.temp_cool && temp_heat === findLastValue.temp_heat &&
             occupied === findLastValue.occupied && relay_status === findLastValue.relay_status &&
@@ -35,21 +67,27 @@ app.post('/SMT-400', async(req, res) => {
             }
             return res.json({ result: "ok" });
         }
+
+
+    // Change Value Update Here
+
         const MsgID = uuid(15);
-        req.MsgID = MsgID;
+        req.body.MsgID = MsgID;
         const deviceParam = new DeviceParam(req.body);
         const result = await deviceParam.save();
         if (!result) {
             return res.json({ message: "Not Connected to database" });
         }
-        var arrayValue = ['mac', 'equip_mode', 'fan_speed', 'temp_cool', 'temp_heat', 'occupied', 'relay_status', 'wifi_lost'];
-        const newArray = [ ['MsgID', MsgID]  ]
+        var arrayValue = ['equip_mode', 'fan_speed', 'temp_cool', 'temp_heat', 'occupied', 'relay_status', 'wifi_lost'];
+        const newArray = [
+            ['MsgID', MsgID]
+        ]
         arrayValue.map((data) => {
-            if (! (findLastValue[data] == result[data])) {
-                return newArray.push([ data, result[data] ])
+            if (!(findLastValue[data] == result[data])) {
+                return newArray.push([data, result[data]])
             }
         });
-         const finalResult = _.object(newArray)
+        const finalResult = _.object(newArray)
         return res.json(finalResult);
     } catch (error) {
         res.json(error);
@@ -83,21 +121,16 @@ app.post('/SMT-400', async(req, res) => {
 
 // mob or web or device get Value
 
-// app.get('/SMT-400/:id', async(req, res) => {
-//     try {
-//         const id = req.params.id;
-//         const result = await DeviceParam.find({ mac: id });
-//         const data = result.reverse()[0];
-//         if (data.MsgID === "") {
-//             return res.json(data);
-//         }
-//         const { MsgID, equip_mode, fan_speed, temp_cool, temp_heat, occupied, relay_status, wifi_lost } = data;
-//         const resultData = { MsgID, equip_mode, fan_speed, temp_cool, temp_heat, occupied, relay_status, wifi_lost };
-//         res.json(resultData);
-//     } catch (error) {
-//         res.json(error);
-//     }
-// });
+app.get('/SMT-400/:id', async(req, res) => {
+    try {
+        const id = req.params.id;
+        const result = await DeviceParam.find({ mac: id });
+        const data = result.reverse()[0];
+        res.json(data);
+    } catch (error) {
+        res.json(error);
+    }
+});
 
 
 app.listen(port, () => {
